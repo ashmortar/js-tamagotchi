35ce02b69010ad3ace27d7e5b3e4faa60d590eea
Aaron Ross and Jun Li <aaron@>
2018-02-12 15:14:30 -0800

    add basic front end mockup and working status bars

--- build/js/app.js
+++ build/js/app.js
@@ -23,6 +23,8 @@ var Tamagotchi = function () {
     this.age = 0;
     this.counter = 0;
     this.isDead = false;
+    this.maxHP = 10;
+    this.currentHP = 10;
     this.favoriteFood = 'meat'; //foodTypes[Math.floor(Math.random() * (foodTypes.length))];
     this.favoritePlay = 'tug-of-war'; //playTypes[Math.floor(Math.random() * (playTypes.length))];
   }
@@ -39,59 +41,92 @@ var Tamagotchi = function () {
         _this.counter++;
         if (_this.counter % 10 === 0) {
           _this.age++;
+          _this.maxHP = 10 + 10 * _this.age;
         }
-        if (_this.foodLevel < 0 || _this.happinessLevel < 0 || _this.restLevel < 0) {
+        if (_this.happinessLevel < 0 || _this.happinessLevel > 10) {
+          _this.currentHP -= 1;
+        }
+        if (_this.restLevel < 0 || _this.restLevel > 10) {
+          _this.currentHP -= 1;
+        }
+        if (_this.foodLevel < 0 || _this.foodLevel > 10) {
+          _this.currentHP -= 1;
+        }
+        if (_this.currentHP <= 0) {
           _this.isDead = true;
         }
       }, 1000);
     }
   }, {
+    key: 'runChecks',
+    value: function runChecks() {
+      if (this.currentHP > this.maxHP) {
+        this.currentHP = this.maxHP;
+      }
+    }
+  }, {
     key: 'feed',
     value: function feed(food) {
+      if (this.isDead) {
+        return this.name + ' has died due to your inability to care for it :(';
+      }
       if (food === this.favoriteFood) {
         this.foodLevel += 5;
-        this.happinessLevel += 1;
-        return this.name + ' LOVED ' + food + '! Its happiness level increased 1 and its food level increased 5';
+        this.currentHP += 1;
+        this.runChecks();
+        return this.name + ' LOVED ' + food + '! Its health increased 1 and its food level increased 5';
       } else if (this.favoriteFood === 'vegetable' && food === 'meat') {
         this.foodLevel -= 1;
-        this.happinessLevel -= 1;
-        return this.name + ' HATED ' + food + '! Its happiness level decreased 1 and its food level decreased 1';
+        this.currentHP -= 1;
+        this.runChecks();
+        return this.name + ' HATED ' + food + '! Its health decreased 1 and its food level decreased 1';
       } else if (this.favoriteFood === 'meat' && food === 'vegetable') {
         this.foodLevel -= 1;
-        this.happinessLevel -= 1;
-        return this.name + ' HATED ' + food + '! Its happiness level decreased 1 and its food level decreased 1';
+        this.currentHP -= 1;
+        this.runChecks();
+        return this.name + ' HATED ' + food + '! Its health decreased 1 and its food level decreased 1';
       } else {
         this.foodLevel += 1;
+        this.runChecks();
         return this.name + ' ate ' + food + ' and its food level increased 1';
       }
     }
   }, {
     key: 'play',
     value: function play(_play) {
+      if (this.isDead) {
+        return this.name + ' has died due to your inability to care for it :(';
+      }
       if (_play === this.favoritePlay) {
         this.happinessLevel += 5;
+        this.runChecks();
         return this.name + ' LOVED ' + _play + '! Its happiness level increased 5.';
       } else if (this.favoritePlay === 'fetch' && _play === 'tug-of-war') {
         this.happinessLevel -= 5;
+        this.runChecks();
         return this.name + ' HATED ' + _play + '! Its happiness level decreased 5.';
       } else if (this.favoritePlay === 'tug-of-war' && _play === 'fetch') {
         this.happinessLevel -= 5;
+        this.runChecks();
         return this.name + ' HATED ' + _play + '! Its happiness level decreased 5.';
       } else {
         this.happinessLevel += 1;
+        this.runChecks();
         return this.name + ' played ' + _play + '! Its happiness level increased 1.';
       }
     }
   }, {
     key: 'sleep',
     value: function sleep(hours) {
-      var _this2 = this;
-
-      setTimeout(function () {
-        _this2.restLevel += hours;
-        _this2.foodLevel -= Math.floor(hours / 2);
-        return _this2.name + ' slept for ' + hours + ' ';
-      }, hours * 1000);
+      if (this.isDead) {
+        return this.name + ' has died due to your inability to care for it :(';
+      }
+      this.restLevel += hours;
+      this.currentHP += Math.ceil(hours / 2);
+      this.foodLevel -= Math.floor(hours / 2);
+      this.runChecks();
+      console.log(this.currentHP);
+      return this.name + ' slept for ' + hours + ' hours';
     }
   }]);
 
@@ -104,5 +139,31 @@ exports.Tamagotchi = Tamagotchi;
 'use strict';
 
 var _tamagotchi = require('./../js/tamagotchi.js');
+
+$(document).ready(function () {
+  //game start function
+  $('#start-play').submit(function (event) {
+    event.preventDefault();
+    var name = $('#name').val();
+    var newTamagotchi = new _tamagotchi.Tamagotchi(name);
+    $('#play-button').hide();
+    $('#play-area').fadeIn();
+    newTamagotchi.startTimer();
+    var timer = setInterval(function () {
+      $('#petName').text(newTamagotchi.name);
+      $('#age').text(newTamagotchi.age);
+      $('#currentHP').prop("style", 'width: ' + newTamagotchi.currentHP / newTamagotchi.maxHP * 100 + '%');
+      $('#foodLevel').prop("style", 'width: ' + newTamagotchi.foodLevel / 10 * 100 + '%');
+      $('#happinessLevel').prop("style", 'width: ' + newTamagotchi.happinessLevel / 10 * 100 + '%');
+      $('#restLevel').prop("style", 'width: ' + newTamagotchi.restLevel / 10 * 100 + '%');
+    }, 500);
+  });
+
+  //feed click function
+
+  //play click function
+
+  //sleep click function
+});
 
 },{"./../js/tamagotchi.js":1}]},{},[2]);
--- js/tamogotchi-interface.js
+++ js/tamogotchi-interface.js
@@ -1,1 +1,28 @@
 import {Tamagotchi} from './../js/tamagotchi.js';
+
+$(document).ready(function() {
+  //game start function
+  $('#start-play').submit(function(event) {
+    event.preventDefault();
+    let name = $('#name').val();
+    let newTamagotchi = new Tamagotchi(name);
+    $('#play-button').hide();
+    $('#play-area').fadeIn();
+    newTamagotchi.startTimer();
+    let timer = setInterval(() => {
+      $('#petName').text(newTamagotchi.name);
+      $('#age').text(newTamagotchi.age);
+      $('#currentHP').prop("style", `width: ${(newTamagotchi.currentHP / newTamagotchi.maxHP)*100}%`);
+      $('#foodLevel').prop("style", `width: ${(newTamagotchi.foodLevel / 10)*100}%`);
+      $('#happinessLevel').prop("style", `width: ${(newTamagotchi.happinessLevel / 10)*100}%`);
+      $('#restLevel').prop("style", `width: ${(newTamagotchi.restLevel / 10)*100}%`);
+    }, 500);
+  });
+
+  //feed click function
+
+  //play click function
+
+  //sleep click function
+
+});
